ae0c5014f07ff6f94dbc1cb7188a9b569f794e27
jch866 <jch866@126.com>
2022-08-24 22:26:56 +0800

    feat: 利用http-proxy-middleware 进行跨域

--- src/04-router/components/Comingsoon.js
+++ src/04-router/components/Comingsoon.js
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./../css/index.css"
+import request from "./../../request";
+const { getComingsoon } = request;
 export default function Comingsoon() {
+  let [list,setList] = useState([])
+  useEffect(()=>{
+    getComingsoon().then(res=>{
+       //bak_coming.json 猫眼即将上映数据
+       if(res.success){
+        setList(res.coming)
+       }
+    })
+  },[])
+  
   return (
-    <div>Comingsoon</div>
+    <div>
+      <ul className='comingsoon'>
+        {
+          list.map(item=>{
+            let {nm,star,showInfo,id} = item;
+            return (<li key={id} >
+              {nm} -- {star}-{showInfo}
+            </li>)
+          })
+        }
+      </ul>
+    </div>
   )
 }
