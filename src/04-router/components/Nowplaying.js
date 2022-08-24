eb33c8249937779116f54f2fc4d8a4b9f6e38439
jch866 <jch866@126.com>
2022-08-24 20:41:50 +0800

    feat:层层传递路由相关属性和方法的案例

--- src/04-router/components/Nowplaying.js
+++ src/04-router/components/Nowplaying.js
@@ -1,18 +1,18 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink, useHistory } from 'react-router-dom';
 import request from "./../../request";
-const {getFilms} = request;
+const { getFilms } = request;
 //Nowplaying 是写在路由中的，它的父级是路由组件 非Films组件
 export default function Nowplaying(props) {
 
-  let [list,setList] = useState([])
-  useEffect(()=>{
-    getFilms(1).then(res=>{
+  let [list, setList] = useState([])
+  useEffect(() => {
+    getFilms(1).then(res => {
       setList(res.data.films)
     })
-  },[])
+  }, [])
   const Uhistory = useHistory();// hooks写法
-  const handleChangePage = (id)=>{
+  const handleChangePage = (id) => {
     // window.location.href = `#/detail/${id}`;
     //props.history.push(`/detail/${id}`)
     // 1.动态路由传参 
@@ -25,42 +25,37 @@ export default function Nowplaying(props) {
   return (
     <div>
       <ul>
-      {
-        list.map((item,index)=>{
-            return <li key={item.filmId} onClick={()=>{handleChangePage(item.filmId)}}>
-              {/* <NavLink to={'/detail/'+item.filmId}>{item.name}</NavLink> */}
-               {item.name} 
-              </li>
-        })
-      }
+        {
+          list.map((item, index) => {
+            // 没用FilmItem组件前handleChangePage可以得到父传递的路由信息
+            // return <li key={item.filmId} onClick={()=>{handleChangePage(item.filmId)}}>
+            //  {item.name} 
+            // </li>
+
+            // {...props} 这里面包含了路由的相关属性和方法，方便子组件FilmItem 利用
+            return <FilmItem key={item.filmId} {...item} {...props} />
+          })
+        }
       </ul>
     </div>
   )
 }
-
-//动态路由复制连接给其它人不会报错 
+function FilmItem(props) {
+  let { name, filmId } = props;
+  return (<li onClick={() => {
+    props.history.push(`/detail/${filmId}`)
+    //props.history 不存在，可以通过父级传递过来 否则就会报错
+    //TypeError: Cannot read properties of undefined (reading 'push') 
+  }}>{name}</li>)
+}
+//动态路由复制连接给其它人不会报错
 //query传参会报错，拿不到ID
 
 // console.dir(props) props是路由组件传过来的属性
 // {
-//   "history": {
-//       "length": 44,
-//       "action": "POP",
-//       "location": {
-//           "pathname": "/films/nowplaying",
-//           "search": "",
-//           "hash": ""
-//       }
-//   },
-//   "location": {
-//       "pathname": "/films/nowplaying",
-//       "search": "",
-//       "hash": ""
-//   },
-//   "match": {
-//       "path": "/films/nowplaying",
-//       "url": "/films/nowplaying",
-//       "isExact": true,
-//       "params": {}
-//   }
+//   "history": {"length": 44,"action": "POP","location": {"pathname": "/films/nowplaying", "search": "","hash": ""}},
+//   "location": {"pathname": "/films/nowplaying","search": "", "hash": ""},
+//   "match": {"path": "/films/nowplaying","url": "/films/nowplaying","isExact": true,"params": {}}
 // }
+
+//层层传递路由相关属性和方法的案例
