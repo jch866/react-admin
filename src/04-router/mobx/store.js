beb6764ededdc1de6de2bb1949a45b243701bb59
jch866 <jch866@126.com>
2022-09-02 21:09:59 +0800

    feat: 装饰器decorator 和异步mobx处理(runInAction)

--- src/04-router/mobx/store.js
+++ src/04-router/mobx/store.js
@@ -1,4 +1,6 @@
-import { observable, autorun } from 'mobx';
+import { observable,action, autorun,configure, runInAction } from 'mobx';
+import request from './../../request';
+let {getCinemaList} = request;
 let obj = {
     isTabbarShow:true,
     list:[],
@@ -6,4 +8,37 @@ let obj = {
 }
 
 let observObj = observable(obj);
-export default observObj
+// export default observObj
+
+configure({ 
+    enforceActions: 'always' 
+}) //开启严格模式
+
+class Store {
+    @observable isTabbarShow=true
+    @observable list=[]
+    @action changeShow(){
+        this.isTabbarShow = true
+    }
+    @action changeHide(){
+        this.isTabbarShow = false
+    }
+
+    @action getList(){
+        console.log('getlist')
+        getCinemaList().then(res=>{
+            runInAction(()=>{
+                this.list = res.data.cinemas;
+            })
+            
+        }).catch(e=>{
+            console.log(e)
+        })
+    }
+}
+const  store = new Store();
+export default store
+//装饰器和异步mobx处理
+//装饰器为什么不能用于函数： 因为存在函数提升， 而类没有。 
+//装饰器是在编译时执行的，而不是运行时。 可以理解为装饰器就是一个编译时执行的对类进行修饰的函数。  
+//异步处理放在runInAction中
--- src/04-router/views/Cinema.js
+++ src/04-router/views/Cinema.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from 'react'
 // import store from '../redux/store';
 import { connect } from "react-redux"
+import store from "../mobx/store"
 import { cinemachange } from '../redux/actionCreater/CinemaActionCreate';
 import { Button, Space,DotLoading,SpinLoading,NavBar,Toast } from "antd-mobile"
 import { SearchOutline, MoreOutline, CloseOutline } from 'antd-mobile-icons'
+import { autorun } from 'mobx';
 
 function Cinema(props) {
-
-  // let [list, setlist] = useState(store.getState().CinemaListReducer.list);
-  let { list, cityname, cinemachange } = props;
+  let [list, setlist] = useState([]);
+  // let { list, cityname, cinemachange } = props;
   useEffect(() => {
     // let list = store.getState().CinemaListReducer.list;
-    if (list.length === 0) {
+    if (store.list.length === 0) {
+      console.log(store)
+      store.getList();
       //发布
       //store.dispatch(cinemachange())
-      cinemachange()
+      // cinemachange()
+    }
+    let unsubscribe = autorun(()=>{
+      console.log(store.list)
+      setlist(store.list)
+    })
+    return ()=>{
+      unsubscribe()
     }
     // else {
     //   console.log('store 缓存')
@@ -28,7 +38,7 @@ function Cinema(props) {
     //   // 取消当前订阅 优化性能
     //   unsubscribe()
     // }
-  }, [list, cityname, cinemachange]);
+  }, []);
   const right = (
     <div style={{ fontSize: 24 }}>
       <Space style={{ '--gap': '16px' }}>
@@ -46,7 +56,7 @@ function Cinema(props) {
   // let [cityname, setcityname] = useState(store.getState().CityReducer.cityName);
   const leftCon = (<div onClick={() => {
       props.history.push('/city')}}>
-      {cityname}
+      {/* {cityname} */}
     </div>)
   return (
     <div>
@@ -81,13 +91,14 @@ function Cinema(props) {
     </div>
   )
 }
-const mapStateToProps = (state) => {
-  return {
-    list: state.CinemaListReducer.list,
-    cityname: state.CityReducer.cityname
-  }
-}
-const mapDispatchToProps = {
-  cinemachange
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Cinema) 
+// const mapStateToProps = (state) => {
+//   return {
+//     list: state.CinemaListReducer.list,
+//     cityname: state.CityReducer.cityname
+//   }
+// }
+// const mapDispatchToProps = {
+//   cinemachange
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(Cinema) 
+export default Cinema
