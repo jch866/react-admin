62a3f0d7ebd7e63210c66dde5417c0c0ba311dc0
jch866 <jch866@126.com>
2023-01-02 14:11:55 +0800

    feat:异步更新mobx容器状态的3种方法;

--- src/06-Mobx/stores/mobx-demo.js
+++ src/06-Mobx/stores/mobx-demo.js
@@ -9,11 +9,30 @@ class StoreDemo {
     @observable num = 0;
     @observable count = 10;
     @observable price = 5;
+    @observable foo = 'foo';
     @computed get total(){
         return this.price * this.count;
     }
     @action.bound modify(){
         this.num = 66;
+    }
+    @action.bound asyncChange(){
+        setTimeout(()=>{
+            // this.count = 500; // 严格模式下可能会报错 待验证 不推荐使用这种方式
+            // 1.重新定个action,用action来操作要修改的数据  业务比较复杂，推荐第一种
+            this.changeCount()
+            // 2.直接调用action 函数 立即执行
+            action('changeFoo',()=>{
+                this.foo = 'hello'
+            })() // 这里加() 立即调用
+            // 3. runInAction 
+            runInAction(()=>{
+                this.foo = 'world'
+            })
+        },500)
+    }
+    @action.bound changeCount(){
+        this.count = 500;
     }
     constructor() {
         //mobx 6版本加了 makeObservable  autorun才有效果
@@ -29,6 +48,11 @@ autorun(() => {
 runInAction(()=>{
     store.num = 88;
 })
+autorun(() => {
+    console.log('StoreDemo autorun foo: ', store.foo)
+})
 store.modify();
 store.num = 100;
 console.log('computed total: ',store.total)
+
+store.asyncChange();
